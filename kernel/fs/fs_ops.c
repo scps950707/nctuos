95d68ad17bbc65ca6c63a86dd1e16c0d7066b836
scps950707 <scps950707@gmail.com>
2018-06-04 23:37:49 +0800

    fs_ops currently stable

--- kernel/fs/fs_ops.c
+++ kernel/fs/fs_ops.c
@@ -48,17 +48,19 @@ extern struct fs_dev fat_fs;
 int fat_mount(struct fs_dev *fs, const void* data)
 {
 	FATFS *obj = (FATFS*)fs->data;
-	if(fs->path[0]=='\0')
-	{
-		printk("fat_mount fs->path==NULL\n");
-	}
-	return f_mount(obj,fs->path,1);
+	/* if(fs->path[0]=='\0') */
+	/* { */
+	/* 	printk("fat_mount fs->path==NULL\n"); */
+	/* } */
+	int res = f_mount(obj,fs->path,1);
+	return -res;
 }
 
 /* Note: Just call f_mkfs at root path '/' */
 int fat_mkfs(const char* device_name)
 {
-	f_mkfs("/",0,0);
+	int res = f_mkfs("/",0,0);
+	return -res;
 }
 
 /* Note: Convert the POSIX's open flag to elmfat's flag.
@@ -71,20 +73,37 @@ int fat_open(struct fs_fd* file)
 	BYTE mode=0;
 
 	if(file->flags==O_RDONLY)
+	{
 		mode |= FA_READ;
+	}
 	if(file->flags&O_WRONLY)
+	{
 		mode |= FA_WRITE;
+	}
 	if(file->flags&O_RDWR)
+	{
 		mode |= (FA_READ|FA_WRITE);
+	}
 	if((file->flags&O_CREAT)&&(file->flags&O_TRUNC))
+	{
+		file->size=0;
 		mode |= FA_CREATE_ALWAYS;
+	}
 	else if(file->flags&O_CREAT)
+	{
+		file->size=0;
 		mode|=FA_CREATE_NEW;
+	}
 
+	file->pos=0;
 	res = f_open((FIL*)file->data,file->path,mode);
 
 	if(file->flags&O_APPEND)
+	{
 		f_lseek((FIL*)file->data,((FIL*)file->data)->obj.objsize);
+		/* printk("((FIL*)file->data)->obj.objsize:%d\n",((FIL*)file->data)->obj.objsize); */
+		/* printk("file->size:%d\n",file->size); */
+	}
 	return -res;
 }
 
@@ -97,16 +116,27 @@ int fat_read(struct fs_fd* file, void* buf, size_t count)
 {
 	UINT br;
 	int res = f_read((FIL*)file->data,buf,count,&br);
-	return -res;
+	return (res == FR_OK) ? br : -res;
 }
 int fat_write(struct fs_fd* file, const void* buf, size_t count)
 {
+	UINT bw;
+	int res = f_write((FIL*)file->data,buf,count,&bw);
+	file->pos += bw;
+	if(file->pos>file->size)
+		file->size=file->pos;
+
+	return (res == FR_OK) ? bw : -res;
 }
 int fat_lseek(struct fs_fd* file, off_t offset)
 {
+	int res = f_lseek((FIL*)file->data,offset);
+	return -res;
 }
 int fat_unlink(struct fs_fd* file, const char *pathname)
 {
+	int res = f_unlink(pathname);
+	return -res;
 }
 
 struct fs_ops elmfat_ops = {
